1de8ece30c511a5ccfe9e0256dec27046f94a91a
BitNexuz <54295481+BitNexuz@users.noreply.github.com>
2021-05-10 16:12:38 -0300

    TarefaFlocking

---
+++ AulaIA/Assets/Scripts/Flock.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public FlockManager myManager;     //chama o flockmanager
+    public float speed;                //configura a velocidade dos peixes
+    bool turning = false;
+
+
+    void Start()
+    {
+        speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);    //Importa as configurações do manager para a velocidade  
+    }
+
+
+    void Update()
+    {
+        Bounds b = new Bounds(myManager.transform.position, myManager.swinLimits * 2); 
+
+        RaycastHit hit = new RaycastHit();                                             //cria um raycast para os peixes
+        Vector3 direction = myManager.transform.position - transform.position;         
+        
+        if(!b.Contains(transform.position))                                                 //faz com que os peixes n colidam
+        {
+            turning = true;
+            direction = myManager.transform.position - transform.position;
+        }
+        else if(Physics.Raycast(transform.position, this.transform.forward * 50, out hit))
+        {
+            turning = true;
+            direction = Vector3.Reflect(this.transform.forward, hit.normal);
+        }
+        else
+            turning = false;
+
+        if(turning)       
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), myManager.rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            if (Random.Range(0, 100) < 10)      //gera uma velocidade de rotação para que o peixe retorne ao grupo
+                speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
+            if(Random.Range(0, 100)< 20) 
+                ApplyRules();
+        }
+       
+        transform.Translate(0, 0, Time.deltaTime * speed);        //movimento dos peixes
+    }
+
+    void ApplyRules()
+    {
+        GameObject[] gos;
+        gos = myManager.allFish;           //importa as informações do allfish
+
+        Vector3 vcentro = Vector3.zero;    //encontra o ponto central dos peixes
+        Vector3 vavoid = Vector3.zero;     //anula colisão
+        float gSpeed = 0.01f;              //faz com que movimente
+        float nDistance;                   //checa a distância entre eles
+        int groupSize = 0;                 //se o distanciamento entre eles for grande, gera outro grupo
+   
+        foreach(GameObject go in gos)      
+        {
+            if(go != this.gameObject)
+            {
+                nDistance = Vector3.Distance(go.transform.position, this.transform.position);
+                if(nDistance <= myManager.neighbourDistance)
+                {
+                    vcentro += go.transform.position;
+                    groupSize++;
+
+                    if(nDistance < 1.0)
+                    {
+                        vavoid = vavoid + (this.transform.position - go.transform.position);
+                    }
+
+                    Flock anotherFlock = go.GetComponent<Flock>();
+                    gSpeed = gSpeed + anotherFlock.speed;
+
+                }
+            }
+        }
+        if(groupSize>0)                     //aplica rotações e verifica se o grupo é maior que zero
+        {
+            vcentro = vcentro / groupSize + (myManager.goalPos - this.transform.position);
+            speed = gSpeed / groupSize;
+
+            Vector3 direction = (vcentro + vavoid) - transform.position;
+            if(direction != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), myManager.rotationSpeed * Time.deltaTime);
+            }
+
+
+        }
+    
+    }
+}
+
---
+++ AulaIA/Assets/Scripts/FlockManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    //configura as variaveis
+    public GameObject fishPrefab;                          //importa prefab do peixe
+    public int numFish = 20;                               //configura a quantidade de peixes
+    public GameObject[] allFish;                           //array para colocar mais peixes
+    public Vector3 swinLimits = new Vector3(5, 5, 5);      //limite de espaço
+    public Vector3 goalPos;
+
+    [Header("Configurações do Cardume")]
+    [Range(0.0f, 5.0f)]                                    
+    public float minSpeed = 0;                                 
+    [Range(0.0f, 5.0f)]                                    //faz a configuração da velocidade 
+    public float maxSpeed = 0;
+    [Range(1.0f, 10.0f)]                                   //faz a configuração do distanciamento entre peixes
+    public float neighbourDistance;
+    [Range(0.0f, 5.0f)]                                    //configura a velocidade de rotação
+    public float rotationSpeed;
+
+
+
+    private void Start()
+    {
+        allFish = new GameObject[numFish];                 
+        for(int i = 0; i < numFish; i++)                   //gera peixes aleatoriamente de acordo com a posição dos outros peixes
+        {
+            Vector3 pos = this.transform.position + new Vector3(Random.Range(-swinLimits.x, swinLimits.x), Random.Range(-swinLimits.y, swinLimits.y), Random.Range(-swinLimits.z, swinLimits.z));
+            allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
+            allFish[i].GetComponent<Flock>().myManager = this;
+        }
+        goalPos = this.transform.position;
+    }
+
+    private void Update()
+    {
+        goalPos = this.transform.position + new Vector3(Random.Range(-swinLimits.x, swinLimits.x), Random.Range(-swinLimits.y, swinLimits.y), Random.Range(-swinLimits.z, swinLimits.z));  
+        if (Random.Range(0, 100) < 10)                                                                                                                                                     //muda o goal position se o random for menor que 10
+        {
+            goalPos = this.transform.position + new Vector3(Random.Range(-swinLimits.x, swinLimits.x), Random.Range(-swinLimits.y, swinLimits.y), Random.Range(-swinLimits.z, swinLimits.z));
+        }
+    
+    }
+}
